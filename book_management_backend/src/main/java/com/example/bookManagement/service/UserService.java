e8789b09770ec3c19f250e0accb03128449159ac
Avishka Wijesinghe <avishkapraveen02@gmail.com>
2025-07-15 23:05:22 +0530

    update

---
+++ book_management_backend/src/main/java/com/example/bookManagement/service/UserService.java
@@ -0,0 +1,5 @@
+package com.example.bookManagement.service;
+
+public interface UserService {
+
+}
---
+++ book_management_backend/src/main/java/com/example/bookManagement/service/UserServiceImpl.java
@@ -0,0 +1,7 @@
+package com.example.bookManagement.service;
+
+public class UserServiceImpl implements UserService {
+
+    
+
+}
